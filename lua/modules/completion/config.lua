59d99a0ac2e377dbe4b55c30adead1e279d8134b
yuchanns <me@yuchanns.xyz>
2023-04-24 22:59:38 +0800

    (codegpt): override command explain

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -57,6 +57,13 @@ function config.codegpt()
   vim.g["codegpt_openai_api_provider"] = "azure"
   vim.g["codegpt_chat_completions_url"] =
     "https://yuchanns-openai.openai.azure.com/openai/deployments/gpt4/chat/completions?api-version=2023-03-15-preview"
+  -- overrides
+  vim.g["codegpt_commands_defaults"] = {
+    ["explain"] = {
+      user_message_template = "Explain the following {{language}} code: ```{{filetype}}\n{{text_selection}}``` Explain it using Chinese as if you were explaining to another developer.",
+      callback_type = "text_popup",
+    },
+  }
   -- custom commands
   vim.g["codegpt_commands"] = {
     -- compatible to yetone's [openai-translator](https://github.com/yetone/openai-translator)
