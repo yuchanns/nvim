f4f1109fdc023475a5e835041f08ede5e51ebab7
yuchanns <me@yuchanns.xyz>
2024-05-28 09:49:19 +0800

    (init): gui font

--- init.lua
+++ init.lua
@@ -2,5 +2,8 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 --[[ vim.opt.spell = true
 vim.opt.spelllang = "en_us,cjk" ]]
+if vim.g.neovide then
+  vim.o.guifont = "BerkeleyMono Nerd Font:h11"
+end
 
 require("core")
