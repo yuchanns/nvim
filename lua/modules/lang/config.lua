2fa89331923520cae813e27598f69d1b808d62b8
yuchanns <me@yuchanns.xyz>
2024-08-18 18:20:50 +0800

    (lang): select block based on treesitter

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -37,6 +37,15 @@ function config.nvim_treesitter()
         },
       },
     },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "gnn", -- set to `false` to disable one of the mappings
+        node_incremental = ".",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
+    },
   })
 end
 
