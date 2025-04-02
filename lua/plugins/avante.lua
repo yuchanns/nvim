37e3cb57a949de1e1626429e88a7403086ec64b3
Hanchin Hsieh <me@yuchanns.xyz>
2025-04-02 19:49:48 +0800

    chore(avante): comment out img-clip.nvim plugin config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -11,7 +11,7 @@ return {
     provider = "copilot",
     auto_suggestions_provider = "azure",
     copilot = {
-      model = "claude-3.7-sonnet",
+      model = "claude-3.5-sonnet",
       temperature = 1,
       max_tokens = 20000,
     },
@@ -38,6 +38,8 @@ return {
     behaviour = {
       support_paste_from_clipboard = true,
       auto_suggestions = false,
+      enable_cursor_planning_mode = true,
+      enable_claude_text_editor_tool_mode = true,
     },
     mappings = {
       ask = "ca",
@@ -68,23 +70,23 @@ return {
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "ibhagwan/fzf-lua", -- for file_selector provider fzf
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
+    -- {
+    --   -- support for image pasting
+    --   "HakonHarnes/img-clip.nvim",
+    --   event = "VeryLazy",
+    --   opts = {
+    --     -- recommended settings
+    --     default = {
+    --       embed_image_as_base64 = false,
+    --       prompt_for_file_name = false,
+    --       drag_and_drop = {
+    --         insert_mode = true,
+    --       },
+    --       -- required for Windows users
+    --       use_absolute_path = true,
+    --     },
+    --   },
+    -- },
     {
       -- Make sure to set this up properly if you have lazy=true
       "MeanderingProgrammer/render-markdown.nvim",
