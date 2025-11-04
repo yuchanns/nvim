d00302ade21413216c22b565aa90c27068775d14
Hanchin Hsieh <me@yuchanns.xyz>
2025-11-04 20:31:56 +0800

    lsp(swift): disable swift

--- lua/lsp/swift.lua
+++ lua/lsp/swift.lua
@@ -1,16 +1,14 @@
 local system = require "utils.system"
 local lsp = require "utils.lsp"
 
-local lspconfig = require "lspconfig"
-
-if not system.is_executable "sourcekit-lsp" then return end
-
-lsp.config("sourcekit", {
-    capabilities = {
-        workspace = {
-            didChangeWatchedFiles = {
-                dynamicRegistration = true,
-            },
-        },
-    },
-})
+-- if not system.is_executable "sourcekit-lsp" then return end
+--
+-- lsp.config("sourcekit", {
+--     capabilities = {
+--         workspace = {
+--             didChangeWatchedFiles = {
+--                 dynamicRegistration = true,
+--             },
+--         },
+--     },
+-- })
