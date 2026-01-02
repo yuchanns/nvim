3805112d2ff5998153758286fba92963f241faad
Hanchin Hsieh <me@yuchanns.xyz>
2026-01-02 16:21:27 +0800

    init: use venv as global python3 prog

--- init.lua
+++ init.lua
@@ -1,1 +1,7 @@
+local venv_python = vim.fn.stdpath "config" .. "/.venv/bin/python"
+
+if vim.fn.executable(venv_python) == 1 then
+    vim.g.python3_host_prog = venv_python
+end
+
 require "core.lazy"
